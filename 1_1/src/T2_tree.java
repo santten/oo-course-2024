8fd2a6a3865c052e0f949796ef88739eac764b22
santten <santtusaaranen@gmail.com>
2024-10-27 19:38:14 +0200

    1.1 task 2

---
+++ 1_1/src/T2_tree.java
@@ -0,0 +1,9 @@
+public class T2_tree {
+    public static void main(String[] args) {
+        System.out.println("    *");
+        System.out.println("   ***");
+        System.out.println("  *****");
+        System.out.println(" *******");
+        System.out.println(" ");
+    }
+}
